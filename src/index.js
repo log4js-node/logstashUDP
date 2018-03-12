1d9e64e7cd432be3de3e6bd981390b50124368c3
huangyichao <huangyichao@baidu.com>
2018-03-12 16:24:07 +0800

    feat: add babel to support previous node and use node 8 to develop

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-'use strict';
-
+// There is something wrong in the log4js.configuration to cause Math not found if import all lodash here
+const _ = require('lodash/core');
+const debug = require('debug')('log4js:logstashUDP');
 const dgram = require('dgram');
 const util = require('util');
 
 function sendLog(udp, host, port, logObject) {
+  debug('Log being sent over UDP');
   const buffer = Buffer.from(JSON.stringify(logObject));
 
-  udp.send(buffer, 0, buffer.length, port, host, (err) => {
+  udp.send(buffer, 0, buffer.length, port, host, err => {
     if (err) {
-      console.error('log4js.logstashUDP - %s:%p Error: %s', host, port, util.inspect(err)); // eslint-disable-line
+      console.error(`log4js.logstashUDP - ${host}:${port} Error: ${util.inspect(err)}.`);
     }
   });
+}
+
+function checkArgs(argsValue, logUnderFields) {
+  if ((!argsValue) || (argsValue === 'both')) {
+    return true;
+  }
+
+  if (logUnderFields && (argsValue === 'fields')) {
+    return true;
+  }
+
+  if ((!logUnderFields) && (argsValue === 'direct')) {
+    return true;
+  }
 }
 
 function logstashUDP(config, layout) {
@@ -21,47 +37,31 @@ function logstashUDP(config, layout) {
     config.fields = {};
   }
 
-  function checkArgs(argsValue, logUnderFields) {
-    if ((!argsValue) || (argsValue === 'both')) {
-      return true;
-    }
-
-    if (logUnderFields && (argsValue === 'fields')) {
-      return true;
-    }
-
-    if ((!logUnderFields) && (argsValue === 'direct')) {
-      return true;
-    }
-
-    return false;
+  /*
+  https://gist.github.com/jordansissel/2996677
+  {
+  'message'  => 'hello world',
+  '@version'  => '1',
+  '@timestamp' => '2014-04-22T23:03:14.111Z',
+  'type'    => 'stdin',
+  'host'    => 'hello.local'
   }
-
+  @timestamp is the ISO8601 high-precision timestamp for the event.
+  @version is the version number of this json schema
+  Every other field is valid and fine.
+  */
   function log(loggingEvent) {
-    /*
-     https://gist.github.com/jordansissel/2996677
-     {
-     'message'  => 'hello world',
-     '@version'  => '1',
-     '@timestamp' => '2014-04-22T23:03:14.111Z',
-     'type'    => 'stdin',
-     'host'    => 'hello.local'
-     }
-     @timestamp is the ISO8601 high-precision timestamp for the event.
-     @version is the version number of this json schema
-     Every other field is valid and fine.
-     */
-
     const fields = {};
-    Object.keys(config.fields).forEach((key) => {
-      fields[key] = typeof config.fields[key] === 'function' ? config.fields[key](loggingEvent) : config.fields[key];
+    _.keys(config.fields).forEach(key => {
+      fields[key] = typeof config.fields[key] === 'function'
+        ? config.fields[key](loggingEvent)
+        : config.fields[key];
     });
 
-    /* eslint no-prototype-builtins:1,no-restricted-syntax:[1, "ForInStatement"] */
     if (loggingEvent.data.length > 1) {
       const secondEvData = loggingEvent.data[1];
       if ((secondEvData !== undefined) && (secondEvData !== null)) {
-        Object.keys(secondEvData).forEach((key) => {
+        _.keys(secondEvData).forEach(key => {
           fields[key] = secondEvData[key];
         });
       }
@@ -81,9 +81,7 @@ function logstashUDP(config, layout) {
     }
 
     if (checkArgs(config.args, false)) {
-      Object.keys(fields).forEach((key) => {
-        logObject[key] = fields[key];
-      });
+      _.forEach(fields, (value, key) => logObject[key] = value);
     }
 
     sendLog(udp, config.host, config.port, logObject);
@@ -93,6 +91,7 @@ function logstashUDP(config, layout) {
     udp.close(cb);
   };
 
+  debug('Appender set and returned.');
   return log;
 }
 
@@ -101,7 +100,6 @@ function configure(config, layouts) {
   if (config.layout) {
     layout = layouts.layout(config.layout.type, config.layout);
   }
-
   return logstashUDP(config, layout);
 }
 
--- test/tap/index-test.js
+++ test/tap/index-test.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const debug = require('debug')('log4js:test.logstashUDP');
 const test = require('tap').test;
 const sandbox = require('@log4js-node/sandboxed-module');
 const appender = require('../../lib');
 
 function setupLogging(category, options) {
   const udpSent = {};
-  const socket = { closed: false };
+  const socket = {closed: false};
 
   const fakeDgram = {
     createSocket: function () {
@@ -59,8 +60,8 @@ function setupLogging(category, options) {
   options = options || {};
   options.type = '../../../lib';
   log4js.configure({
-    appenders: { logstash: options },
-    categories: { default: { appenders: ['logstash'], level: 'trace' } }
+    appenders: {logstash: options},
+    categories: {default: {appenders: ['logstash'], level: 'trace'}}
   });
 
   return {
@@ -73,13 +74,13 @@ function setupLogging(category, options) {
   };
 }
 
-test('logstashUDP appender', (batch) => {
-  batch.test('should export a configure function', (t) => {
+test('logstashUDP appender', batch => {
+  batch.test('should export a configure function', t => {
     t.type(appender.configure, 'function');
     t.end();
   });
 
-  batch.test('a UDP packet should be sent', (t) => {
+  batch.test('a UDP packet should be sent', t => {
     const setup = setupLogging('myCategory', {
       host: '127.0.0.1',
       port: 10001,
@@ -125,7 +126,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('udp errors', (t) => {
+  batch.test('udp errors', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -135,14 +136,14 @@ test('logstashUDP appender', (batch) => {
     setup.results.error = new Error('Some sort of UDP thing');
     setup.logger.log('trace', 'Log event #1');
 
-    t.test('should be sent to console.error', (assert) => {
-      assert.equal(setup.console.message, 'log4js.logstashUDP - %s:%p Error: %s');
+    t.test('should be sent to console.error', assert => {
+      assert.ok(setup.console.message.startsWith('log4js.logstashUDP - 127.0.0.1:10001 Error: '));
       assert.end();
     });
     t.end();
   });
 
-  batch.test('default options', (t) => {
+  batch.test('default options', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -155,13 +156,13 @@ test('logstashUDP appender', (batch) => {
     t.equal(json.type, 'myLogger');
     t.equal(
       JSON.stringify(json.fields),
-      JSON.stringify({ level: 'TRACE', category: 'myLogger' })
+      JSON.stringify({level: 'TRACE', category: 'myLogger'})
     );
 
     t.end();
   });
 
-  batch.test('using a custom layout', (t) => {
+  batch.test('using a custom layout', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -182,7 +183,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('configuration can include functions to generate field values at run-time', (t) => {
+  batch.test('configuration can include functions to generate field values at run-time', t => {
     const setup = setupLogging('myCategory', {
       host: '127.0.0.1',
       port: 10001,
@@ -205,14 +206,14 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('extra fields should be added to the fields structure', (t) => {
+  batch.test('extra fields should be added to the fields structure', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
       type: 'logstashUDP',
       category: 'myLogger'
     });
-    setup.logger.log('trace', 'Log event #1', { extra1: 'value1', extra2: 'value2' });
+    setup.logger.log('trace', 'Log event #1', {extra1: 'value1', extra2: 'value2'});
 
     const json = JSON.parse(setup.results.buffer.toString());
     const fields = {
@@ -225,7 +226,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('use direct args', (t) => {
+  batch.test('use direct args', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -234,7 +235,7 @@ test('logstashUDP appender', (batch) => {
       args: 'direct'
     });
 
-    setup.logger.log('info', 'Log event with fields', { extra1: 'value1', extra2: 'value2' });
+    setup.logger.log('info', 'Log event with fields', {extra1: 'value1', extra2: 'value2'});
     const json = JSON.parse(setup.results.buffer.toString());
 
     t.equal(json.extra1, 'value1');
@@ -243,7 +244,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('use fields args', (t) => {
+  batch.test('use fields args', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -252,7 +253,7 @@ test('logstashUDP appender', (batch) => {
       args: 'fields'
     });
 
-    setup.logger.log('info', 'Log event with fields', { extra1: 'value1', extra2: 'value2' });
+    setup.logger.log('info', 'Log event with fields', {extra1: 'value1', extra2: 'value2'});
     const json = JSON.parse(setup.results.buffer.toString());
 
     t.equal(json.extra1, undefined);
@@ -262,7 +263,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('Send null as argument', (t) => {
+  batch.test('Send null as argument', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -278,7 +279,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('Send undefined as argument', (t) => {
+  batch.test('Send undefined as argument', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
@@ -294,7 +295,7 @@ test('logstashUDP appender', (batch) => {
     t.end();
   });
 
-  batch.test('shutdown should close sockets', (t) => {
+  batch.test('shutdown should close sockets', t => {
     const setup = setupLogging('myLogger', {
       host: '127.0.0.1',
       port: 10001,
